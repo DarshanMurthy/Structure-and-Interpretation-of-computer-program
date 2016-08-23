74d18b60a60825b2e3de9f8a01adc6b893c4be72
DarshanMurthy <darshants.darshan1@gmail.com>
2016-08-23 15:17:05 -0700

    Exercise 1.1-Exercise 1.5

--- Chapter.scm
+++ Chapter.scm
@@ -23,3 +23,9 @@
 ;5+4+(2−(3−(6+ 4/5))) / 3(6 − 2)(2 − 7)
 
 
+;Exercise 1.3: Define a procedure that takes three numbers as arguments and 
+;returns the sum of the squares of the two larger numbers.
+
+
+;Exercise 1.4: Observe that our model of evaluation allows for combinations whose operators are compound expres- sions. Use this observation to describe the behavior of the following procedure:
+;(define (a-plus-abs-b a b) ((if (> b 0) + -) a b))
